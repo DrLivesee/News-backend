6ca4d4cbe11e8090cfbf8f7063e1e8446661894c
Evgenii Manaenkov <evgenijmanaenkov@gmail.com>
2024-05-20 15:16:14 +0300

    feat: add comments for user and patch comments

--- src/controllers/comments-controller.ts
+++ src/controllers/comments-controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import Comment, {
   IComment,
   ICommentGetResponse,
+  ICommentsForUserGetResponse,
 } from "@src/models/commentModel";
 
 async function getComments(req: Request, res: Response): Promise<void> {
@@ -31,6 +32,20 @@ async function getCommentsForNews(req: Request, res: Response): Promise<void> {
       );
     }
     res.json({ comments, totalDocuments } as ICommentGetResponse);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+async function getCommentsForUser(req: Request, res: Response): Promise<void> {
+  const userId: string = req.params.userId as string;
+
+  try {
+    let comments: IComment[] = await Comment.find({ userId });
+    const totalDocuments: number = comments.length;
+
+    const ids: string[] = comments.map(comment => comment._id);
+    res.json({ ids, totalDocuments } as ICommentsForUserGetResponse);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -77,6 +92,27 @@ async function patchComment(req: Request, res: Response): Promise<void> {
   }
 }
 
+async function patchComments(req: Request, res: Response): Promise<void> {
+  try {
+    const ids: string[] = req.body.ids;
+    const updatedFields: Partial<IComment> = req.body.updatedFields;
+
+    if (!ids || !Array.isArray(ids) || ids.length === 0 || !updatedFields || typeof updatedFields !== 'object') {
+      res.status(400).json({ message: "Invalid request body" });
+      return;
+    }
+
+    const result = await Comment.updateMany(
+      { _id: { $in: ids } },
+      { $set: updatedFields }
+    );
+
+    res.json(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
 async function deleteComment(req: Request, res: Response): Promise<void> {
   try {
     const id: string = req.params.id;
@@ -92,6 +128,8 @@ export {
   postComment,
   getCommentById,
   patchComment,
+  patchComments,
   deleteComment,
   getCommentsForNews,
+  getCommentsForUser,
 };
